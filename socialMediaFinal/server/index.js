ea590a073e45ba77942a439f9814e9744dfda937
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-27 16:52:45 +0200

    added packages and fixed some issues

--- socialMediaFinal/server/index.js
+++ socialMediaFinal/server/index.js
@@ -49,6 +49,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
+const port = 5500 || process.env.PORT 
+
+app.listen(port, () => {
   console.log("Backend server is running!");
 });
