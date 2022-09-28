eb0f6d00cf764641f602aa3c5955becf9c112bcb
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-29 00:58:40 +0200

    added images

---
+++ lamaBooking/setup/client/src/constant/images.js
@@ -0,0 +1,11 @@
+import img1 from '../images/duplex1.jfif'
+import img2 from '../images/family1.jfif'
+import img3 from '../images/family3.jfif'
+import img4 from "../images/family5.jpeg";
+
+export const images = {
+  img1,
+  img2,
+  img3,
+  img4
+}
