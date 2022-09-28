829d1b231d9112eddf0074e943c751ef198db877
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-29 00:46:20 +0200

    created the navbar

--- lamaBooking/setup/client/src/App.js
+++ lamaBooking/setup/client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import { Home } from './pages'
-import {Header } from './components'
+import {Header , Navbar } from './components'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Navbar/>
         <Header/>
         <Routes>
           <Route path='/' component={Home} />
