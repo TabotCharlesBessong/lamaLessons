0f1ad6662fafd9ba84c25455f596d5e96e2ff9d7
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-28 23:03:01 +0200

    added the header

--- lamaBooking/setup/client/src/App.js
+++ lamaBooking/setup/client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter , Switch , Route } from 'react-router-dom'
 import { Home } from './pages'
+import {Header } from './components'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Header/>
         <Switch>
           <Route path='/' component={Home} />
         </Switch>
