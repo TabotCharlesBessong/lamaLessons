1d8682415d9b4117963fe8a95c86ee8e2c224481
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-29 12:45:00 +0200

    updated images and created mock data

---
+++ lamaBooking/setup/client/src/constant/data.js
@@ -0,0 +1,43 @@
+
+import images from './images'
+
+const featured = [
+	{
+		id: 1,
+		name: "Charles",
+		city: "Limbe",
+		price: 54000,
+		rating: 4.6,
+		photos: [images.img1, images.img2, images.img3],
+	},
+	{
+		id: 2,
+		name: "Bessong",
+		city: "Douala",
+		price: 23000,
+		rating: 3.6,
+		photos: [images.img4, images.img5, images.img6],
+	},
+	{
+		id: 1,
+		name: "Tabot",
+		city: "Buea",
+		price: 54000,
+		rating: 4.2,
+		photos: [images.img7, images.img8, images.img9,images.img10],
+	},
+	{
+		id: 4,
+		name: "Junior",
+		city: "Ouesso",
+		price: 11700,
+		rating: 3.2,
+		photos: [images.img11, images.img12],
+	},
+];
+
+const data = {
+  featured
+}
+
+export default data
--- lamaBooking/setup/client/src/constant/images.js
+++ lamaBooking/setup/client/src/constant/images.js
@@ -2,12 +2,28 @@ import img1 from '../images/duplex1.jfif'
 import img2 from '../images/family1.jfif'
 import img3 from '../images/family3.jfif'
 import img4 from "../images/family5.jpeg";
+import img5 from '../images/duplex3.jfif'
+import img6 from '../images/duplex4.jfif'
+import img7 from '../images/hall4.jfif'
+import img8 from '../images/motel6.jfif'
+import img9 from '../images/room1.jfif'
+import img10 from '../images/room2.jfif'
+import img11 from '../images/room3.jfif'
+import img12 from '../images/room4.jfif'
 
 const images = {
   img1,
   img2,
   img3,
-  img4
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12
 }
 
 export default images 
