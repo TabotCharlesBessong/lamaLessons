6ea82bc106d2b785408a78f3a07ef058063ed4d8
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-30 18:40:52 +0200

    added the hotel page

--- lamaBooking/setup/client/src/App.js
+++ lamaBooking/setup/client/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import { Home } from './pages'
+import { Home , Hotel } from './pages'
 import {Header , Navbar, Footer } from './components'
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar/>
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-    </div>
-  )
+		<div>
+			<BrowserRouter>
+				<Navbar />
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/hotel" element={<Hotel />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App
--- lamaBooking/setup/client/src/pages/index.js
+++ lamaBooking/setup/client/src/pages/index.js
@@ -1,2 +1,3 @@
 
 export { default as Home } from './home/Home'
+export { default as Hotel } from "./hotel/Hotel";
