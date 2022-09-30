46454c6bdb478f6b4618973b56f18470564e299d
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-30 18:30:14 +0200

    just created the footer

--- lamaBooking/setup/client/src/App.js
+++ lamaBooking/setup/client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import { Home } from './pages'
-import {Header , Navbar } from './components'
+import {Header , Navbar, Footer } from './components'
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
