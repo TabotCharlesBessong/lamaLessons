3cd6895aa36e22ef00d3016caa42b8cdb69c3c7c
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-01 21:20:09 +0200

    authentication handled

--- socialMedia/client/src/App.js
+++ socialMedia/client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react' 
-import { Home } from './pages'
-// import { Header } from './components'
+import { BrowserRouter , Route, Routes } from 'react-router-dom'
+import { Home, Login, Register } from './pages'
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
-  )
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" component={Home} />
+					<Route path="/login" component={Login} />
+					<Route path="/register" component={Register} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App
--- socialMedia/client/src/pages/login/Login.js
+++ socialMedia/client/src/pages/login/Login.js
@@ -1,10 +1,50 @@
 import React from 'react'
-import './login.css'
+import { useContext, useRef } from "react";
+import "./login.css";
+import { CircularProgress } from "@material-ui/core";
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+export default function Login() {
+	const email = useRef();
+	const password = useRef();
+
+	
+
+	return (
+		<div className="login">
+			<div className="loginWrapper">
+				<div className="loginLeft">
+					<h3 className="loginLogo">Lamasocial</h3>
+					<span className="loginDesc">
+						Connect with friends and the world around you on Lamasocial.
+					</span>
+				</div>
+				<div className="loginRight">
+					<form className="loginBox" onSubmit={()=> console.log('hello') }>
+						<input
+							placeholder="Email"
+							type="email"
+							required
+							className="loginInput"
+							ref={email}
+						/>
+						<input
+							placeholder="Password"
+							type="password"
+							required
+							minLength="6"
+							className="loginInput"
+							ref={password}
+						/>
+						<button className="loginButton" type="submit" >
+							login 
+						</button>
+						<span className="loginForgot">Forgot Password?</span>
+						<button className="loginRegisterButton">
+							create new account
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
-
-export default Login
